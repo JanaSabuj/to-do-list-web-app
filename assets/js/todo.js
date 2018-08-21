1bbe91b0316bf946e10e5254c56c66468781d472
Sabuj <sabujlovescoding@gmail.com>
2018-08-21 16:54:18 +0530

    deleting todos

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,5 +1,17 @@
- 
- $("li").click(function() {
+ alert("hello");
+
+ $("ul").on("click","li",function() {
 	$(this).toggleClass("completed");
  
 });
+
+ $("ul").on("click","span",function(event){
+ 	
+ 	$(this).parent().fadeOut(500,function() {
+ 		$(this).remove();
+ 	});
+event.stopPropagation();
+ });
+
+
+
